0804509a1ca11a1ac56608ae3bea402ebfb52f52
Jena2003 <jena.zubova.03@mail.ru>
2022-06-16 19:52:53 +0700

    first commit

---
+++ client/src/components/Graf.vue
@@ -0,0 +1,75 @@
+<style ></style>
+<template>
+  <div id="app">
+    <d3-network
+      :net-nodes="nodes"
+      :net-links="links"
+      :options="options"
+       :configs="configs"
+    ></d3-network>
+<div class="text">
+  <a>Соавторы:
+   <li>Кратов С.В.</li> 
+   <li> Матерухин А.В.</li>
+   <li> Шварцкоп Н.С.</li>
+  </a>
+  <a>Вы являетесь соавтором для:
+    <li>Шахов В.В. </li>
+   <li> Мигов Д.А.</li> 
+  </a>
+</div>
+    
+  </div>
+</template>
+
+<script>
+import D3Network from "vue-d3-network";
+
+export default {
+  name:'Graf',
+  components: {
+    D3Network,
+  },
+  data() {
+    return {
+      nodes: [
+        { id: 1, name: "Соколова А.Д.",_size:42, _color: 'rgba(1, 68, 97)' },
+        { id: 2, name: "Шахов В.В",_size:42, _color:'rgba(49, 141, 212)'},
+        { id: 3, name: "Таранхова О.Ю.",_size:42,_color: "blue"},
+        { id: 4,name: "Мигов Д.А",_size:42,_color:'rgba(49, 141, 212)'},
+        { id: 5 , name: "Щварцкоп Н.С.",_size:42},
+        { id: 6, name: "Кратов С.В.",_size:42},
+        { id: 7, name: "Матерухин А.В.",_size:42},
+      ],
+      links: [
+        { sid: 1, tid: 2,type: "resolved"},
+        { sid: 1, tid: 4},
+        { sid: 3, tid: 2 },
+        { sid: 6, tid: 1 },
+        { sid: 5, tid: 1 },
+        { sid: 7, tid: 1 },
+        
+      ],
+      options: {
+        force: 3000,
+        nodeSize: 15,
+        nodeLabels: true,
+        linkWidth: 2,
+      },
+      configs: ''
+      
+}}
+}
+</script>
+<style src="vue-d3-network/dist/vue-d3-network.css" >
+@import '../utils/const.less';
+@import '../utils/mixins.less';
+.net_nodes{
+  size: 100px,
+}
+.text{
+  border: none;
+      outline: none;
+  color:#ECEDEE;
+}
+</style>
---
+++ client/src/components/MyArticles.vue
@@ -0,0 +1,157 @@
+<template>
+<div>
+<div class="container">
+   <div class="menu">
+      <router-link class="menu__item" to="/MyProfileScreen">Мой профиль</router-link>
+      <div class= "menu__item menu__item_checked" >Мои статьи</div>
+      <router-link class="menu__item"  to="/EditProfileScreen">Редактировать профиль</router-link>
+      <a href="#openModal" class="menu__item">Выйти из профиля</a>
+    </div>
+<div class="articles">
+   <div>
+      <input @click="visiblemessage =! visiblemessage" type="checkbox" class="checkbox">
+      <label  class="message" v-for="item in items" :key="item.message">
+         {{item.message}}
+      </label>
+      <div v-if=" visiblemessage" class="container">
+         <a>Статья не цитируется</a>
+      </div>
+   </div>
+    <div>
+   <input @click="visiblemessage1 =! visiblemessage1" type="checkbox" class="checkbox">
+      <label  class="message" v-for="item in items" :key="item.message1">
+         {{item.message1}}
+      </label>
+      <div v-if=" visiblemessage1" class="container">
+         <a>Статья не цитируется</a>
+      </div>
+      </div>
+       <div>
+   <input @click="visiblemessage2 =! visiblemessage2" type="checkbox" class="checkbox">
+      <label  class="message" v-for="item in items" :key="item.message2">
+         {{item.message2}}
+      </label>
+      <div v-if="visiblemessage2" class="container">
+         <a>Статья "АНАЛИТИЧЕСКИЙ ОБЗОР СОВРЕМЕННЫХ ИНФОРМАИОННЫХ ТЕХНОЛОГИЙ В ОБЛАСТИ СБОРА
+          <p>  ОБРАБОТКИ И АНАЛИЗА ДАННЫХ, ПРИМИНЯЕМЫХ ДЛЯ МОНИТОРИНГА ЗАГРЯЗНЕНИЙ АТМОСФЕРНОГО ВОЗДУХА"</p></a>
+            <p> Цитиретуся в:</p>
+            <p> Головина Е.Ю. , Волкова О.Г.</p>
+            <p> МОДЕРНИЗАЦИЯ ПРОЦЕССА "КОНТРОЛЬ ЭКОЛОГИЧЕСКОЙ ОБСТАНОВКИ" НА ПРЕДПРИЯТИИ</p>
+      </div>
+      </div>
+       <div>
+   <input @click="visiblemessage3 =! visiblemessage3" type="checkbox" class="checkbox">
+      <label  class="message" v-for="item in items" :key="item.message3">
+         {{item.message3}}
+      </label>
+      <div v-if=" visiblemessage3" class="container">
+         <a>Статья не цитируется</a>
+      </div>
+      </div>
+</div>
+      <button class="profile__button" @click="coauthor">Найти соавторов</button>
+</div>
+
+<a name="graf" v-if="iscoauthor" class="graf">
+      <Graf />
+</a>
+</div>
+</template>
+
+<script>
+import Graf from './Graf.vue'
+//import axios from 'axios'
+export default {
+  name: 'MyArticles',
+  el:'#example-1',
+  data(){
+   return{
+   items: [
+      {message: 'О популяризации наукоемких программных разработок'},
+      {message1: 'Применение нестанционарных сетей в задачах'},
+      {message2: 'Аналитический обзор современных информаионных технологий...'},
+      {message3: 'Беспроводные сенсорные сети, оснащенные средствами получения'}
+   ],
+    article: {
+        id:"",
+        coauthor: "",
+        titleArticles: "",
+        doi: "",
+        content: "",
+        keywords: "",
+        biblecalReference: "",
+        file: ""
+      },
+      visiblemessage: false,
+      visiblemessage1: false,
+      visiblemessage2: false,
+      visiblemessage3: false,
+      iscoauthor: false
+  }},
+  components: {
+    Graf
+  },
+  methods: {
+   coauthor(){
+      this.iscoauthor=true
+   },
+}}
+</script>
+<style lang="less" scoped>
+@import '../utils/const.less';
+@import '../utils/mixins.less';
+.checkbox{
+      border: none;
+      outline: none;
+      background-color: @utility-border-input;
+}
+.articles{
+   display: flex;
+   flex-direction: column;
+   width: 100%;
+   padding: 30px 30px 30px 0;
+    margin-left: 10%;
+}
+.menu{
+     width: 200px;
+    margin-left: 100px;
+    height: 100%;
+    display: flex;
+    margin-top: 30px;
+    background-color: @dark-text-secondary;
+    flex-direction: column;
+    &__item {
+      border: 1px solid @utility-border;
+      padding: 8px 8px 14px;
+      background-color: rgba(1, 68, 97,0.7);
+      color: @utility-white;
+      text-decoration: none;
+      cursor:pointer;
+      &:hover {
+        background-color: rgba(1, 68, 97);
+      box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1), 0px 4px 20px rgba(0, 0, 0, 0.1);
+      } 
+      &_checked {
+      background-color: rgba(1, 68, 97);
+      box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1), 0px 4px 20px rgba(0, 0, 0, 0.1);
+    }
+    }
+}
+.container{
+   background-color: @utility-border;
+}
+.profile__button{
+   display: flex;
+   flex-direction: column;
+     text-align: center;
+      border: none;
+      outline: none;
+      .nice-shadow();
+      margin-top:10px;
+        padding: 5px 5px 5px 5px;
+      border-radius: 10px;
+      background-color: rgba(1, 68, 97,0.8);
+      color: @utility-white;
+}
+
+</style>
